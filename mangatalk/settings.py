19562d37eeb68030253e186a26cb5015ab74df12
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-01-26 06:59:09 +0000

    change[settings.py]: import a couple of libraries and set templates folder routes for apps

--- mangatalk/settings.py
+++ mangatalk/settings.py
@@ -9,12 +9,15 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+import dj_database_url
+if os.path.isfile("env.py"):
+	import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
