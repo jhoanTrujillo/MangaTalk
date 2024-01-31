5948f36c62e4e4ce6b7a3d0292f3c7352639323c
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-01-31 18:29:51 +0000

    Change: replace blog app to forum app to start building the forum

--- mangatalk/settings.py
+++ mangatalk/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-bkmt$+d3mt8fhy&sua140-_1lhiv8=3g7u32ff6nm^c)ikhhiu
 DEBUG = False
 
 # Link of allowed server name spaces to host the app. 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'blog'
+	'forum',
+	'django_summernote',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'mangatalk.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
